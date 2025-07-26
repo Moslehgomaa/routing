561270f9eb528301ba1c607b80c83de1c447e298
Moslehgomaa <Moslehgomaa15@gmail.com>
2025-07-27 02:50:28 +0300

    init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
+import { Notfound } from './components/notfound/notfound';
+import { Gallery } from './components/gallery/gallery';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '' , component: Home , title:'Home' } ,
+    {path: 'home' , component: Home , title:'Home' } ,
+    {path: 'about' , component: About , title:'About'} ,
+    {path: 'gallery' , component: Gallery , title:'Portfolio'} ,
+    {path: 'contact' , component: Contact , title:'Contact'} ,
+    {path: '**' , component: Notfound , title:'Notfound'} ,
+];
---
+++ src/app/components/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id:number;
+    title:string;
+    description:string;
+    imageCover:string;
+    onSale:boolean;
+    price:number;
+    quantity:number;
+    images:string[]
+}
